464797019ff7cb43f8aac3c77dc348e981dabd6a
sok5188 <sok5188@gmail.com>
2023-03-12 16:19:52 +0900

    initialize

---
+++ src/main/java/com/example/emptySaver/EmptySaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.emptySaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptySaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emptySaver/EmptySaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emptySaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptySaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
