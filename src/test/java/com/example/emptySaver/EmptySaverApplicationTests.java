64ae3d0f75ddfbbe33eedfc9fbfd4de347ea72e7
sok5188 <sok5188@gmail.com>
2023-04-17 21:11:31 +0900

    빌드 과정에서 오류 수정

--- src/test/java/com/example/emptySaver/EmptySaverApplicationTests.java
+++ src/test/java/com/example/emptySaver/EmptySaverApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.emptySaver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmptySaverApplicationTests {
 
 	@Test
