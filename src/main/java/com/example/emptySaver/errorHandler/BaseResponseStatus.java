8d640df1db96c424a20eeca27b5b0a72ef205ca6
sok5188 <sok5188@gmail.com>
2023-04-20 22:50:37 +0900

    필요없는 오류코드 삭제 및 필요한 오류코드 추가

--- src/main/java/com/example/emptySaver/errorHandler/BaseResponseStatus.java
+++ src/main/java/com/example/emptySaver/errorHandler/BaseResponseStatus.java
@@ -14,16 +14,10 @@ public enum BaseResponseStatus {
     /**
      * 2000 : Request 오류
      */
-    EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
-    INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
-    INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
-
     POST_USERS_EXISTS_EMAIL(false, 2021, "이미 가입된 이메일입니다."),
-    INVALID_SINGER_ID(false,2220,"유효하지 않은 가수 id입니다."),
-    INVALID_USER_ID(false,2221,"유효하지 않은 회원 id입니다."),
-    INVALID_ALBUM_ID(false,2222,"유효하지 않은 앨범 id입니다."),
-    INVALID_MUSIC_ID(false,2223,"유효하지 않은 음악 id입니다."),
-    INVALID_PLAYLIST_ID(false,2224,"유효하지 않은 플레이리스트 id입니다."),
+    INVALID_EMAIL(false, 2221, "존재하지 않는 이메일입니다."),
+    INVALID_PASSWORD(false, 2222, "유효하지 않은 비밀번호입니다."),
+    INVALID_CHANGE_ATTEMPT_NAME(false, 2223, "유효하지 않은 변경 시도입니다.(이름이 올바르지 않습니다.)"),
     INVALID_SAVE_ATTEMPT(false,2225,"유효하지 않은 저장 시도입니다.(이미 저장되어 있습니다)"),
     INVALID_DELETE_ATTEMPT(false,2225,"유효하지 않은 삭제 시도입니다."),
 
@@ -32,17 +26,8 @@ public enum BaseResponseStatus {
      */
     // Common
     RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
-
-
-    POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
-
     FAILED_TO_LOGIN(false, 3014, "가입된 유저가 아닙니다."),
-
-    /**
-     * 4000 : Database, Server 오류
-     */
-    DATABASE_ERROR(false,4000,"데이터베이스 연결에 실패하였습니다."),
-    SERVER_ERROR(false,4001,"서버와의 연결에 실패하였습니다.");
+    FAILED_TO_SEND_MAIL(false,3111,"메일 전송에 실패했습니다.");
 
 
     private final boolean isSuccess;
