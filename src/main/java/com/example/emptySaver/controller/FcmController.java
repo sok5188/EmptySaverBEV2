2db640f9e3f5c1aecc8d04a9b565558fe67f23e2
sok5188 <sok5188@gmail.com>
2023-05-03 20:53:12 +0900

    tmp for test

--- src/main/java/com/example/emptySaver/controller/FcmController.java
+++ src/main/java/com/example/emptySaver/controller/FcmController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FcmController {
     private final FCMService fcmService;
     @Operation(summary = "알림 전송", description = "현재 로그인 된 유저의 fcm토큰을 이용하여 알림 보내기")
-    @GetMapping("/send")
+    @RequestMapping("/send")
     public ResponseEntity<String> sendNotification(@RequestBody FCMDto fcmDto){
         fcmService.sendNotificationByToken(fcmDto);
         return ResponseEntity.ok("Send Notification");
