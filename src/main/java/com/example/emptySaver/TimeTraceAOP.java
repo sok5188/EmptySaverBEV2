e8686645145844a777086760ab461df822d9f464
sok5188 <sok5188@gmail.com>
2023-07-15 19:41:37 +0900

    불필요한 AOP 컴포넌트 스캔 안되게 수정

--- src/main/java/com/example/emptySaver/TimeTraceAOP.java
+++ src/main/java/com/example/emptySaver/TimeTraceAOP.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Aspect
 @Slf4j
 public class TimeTraceAOP {
